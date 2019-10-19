b1503e0431e9bc13cec504a2665bab93fc1b983e
Ed dos Prazeres <edimilsonpereira.pro@gmail.com>
2019-10-19 03:05:05 -0300

    Temp Colors and Base metrics

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,3 +1,9 @@
 export default {
-
+    primary: "#EC1D24",
+    secundary: "",
+    black: "#21252d",
+    midlack: "#252A33",
+    lightBlack: "#282e39",
+    white: "#fff",
+    transparent: 'transparent'
 }
